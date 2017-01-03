20400708d696a658c82ab27cd8aee821d41326f8
Braden Walters <vc@braden-walters.info>
2017-01-03 21:36:43 +0100

    Removed references to MIT license

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 /**
  * Copyright (C) 2017 Braden Walters
  *
- * This file is licensed under the MIT Expat License. See LICENSE.txt.
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
